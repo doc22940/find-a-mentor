1dfb6a6a6cbf1ac09baf319d47f8811b0dc0c95b
Mooli Morano <moolipit@gmail.com>
2020-12-19 00:37:29 +0000

    update input test

--- src/Me/components/Input/Input.test.js
+++ src/Me/components/Input/Input.test.js
@@ -1,10 +1,12 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import Input from './';
-import { render } from '@testing-library/react';
 
 describe('Input component', () => {
   it('renders properly', () => {
-    const { container } = render(<Input placeholder="test" />);
+    const { container } = render(
+      <Input placeholder="test" value="test" onChange={() => {}} />
+    );
     expect(container.firstChild).toMatchSnapshot();
   });
 });
